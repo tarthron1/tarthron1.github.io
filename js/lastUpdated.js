531a3ea35312079519de793c0b6a227cc6d29825
tarthron1 <kyle.loertscher@gmail.com>
2020-09-17 10:42:48 -0600

    add css styling

---
+++ js/lastUpdated.js
@@ -0,0 +1,3 @@
+document.getElementById("lastUpdated").innerHTML = "Last Updated: " + document.lastModified;
+var date = new Date();
+document.getElementById("currentYear").innerHTML =  "&#169; " + date.getFullYear().toString();
